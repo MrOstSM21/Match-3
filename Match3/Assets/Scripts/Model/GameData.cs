ba6f18201e77292704f41c1d836db586fedff1f4
MrOst <b0bkov21@yandex.ru>
2022-07-18 16:04:33 +0300

    Add UI and images
    -Добавлено графическое оформление и UI.

--- Match3/Assets/Scripts/Model/GameData.cs
+++ Match3/Assets/Scripts/Model/GameData.cs
@@ -6,10 +6,10 @@ public class GameData
     public const int GRID_X = 6;
     public const int GRID_Y = 7;
     public int TileOverlap { get; } = 2;
-    public float Offset { get; } = 0.7f;
+    public float Offset { get; } = 0.6f;
     public float SwapTileSpeed { get; } = 0.004f;
     public float MoveTileSpeed { get; } = 0.04f;
-    public Vector3 StartPoint { get; } = new Vector3(-2.3f, -2.3f, 0f);
+    public Vector3 StartPoint { get; } = new Vector3(-2.0f, -2.3f, 0f);
     public Vector3 PrefabSize { get; } = new Vector3(0.2f, 0.2f, 0f);
 
     public Dictionary<TilesName, int> GetPoints { get; } = new Dictionary<TilesName, int>()
